9d0d40d958fdbd372d517dcfddee37056e790a68
Valeriy <valera.zlb@outlook.com>
2020-02-25 09:33:48 +0300

    Добавление контролов

--- ADGS/ADGS/MainForm.Designer.cs
+++ ADGS/ADGS/MainForm.Designer.cs
@@ -28,20 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menu_main = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.MenuItem_help = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItem_about = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_main.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menu_main
+            // 
+            this.menu_main.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItem_help});
+            this.menu_main.Location = new System.Drawing.Point(0, 0);
+            this.menu_main.Name = "menu_main";
+            this.menu_main.Size = new System.Drawing.Size(800, 24);
+            this.menu_main.TabIndex = 0;
+            this.menu_main.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "status_main";
+            // 
+            // MenuItem_help
+            // 
+            this.MenuItem_help.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItem_about});
+            this.MenuItem_help.Name = "MenuItem_help";
+            this.MenuItem_help.Size = new System.Drawing.Size(44, 20);
+            this.MenuItem_help.Text = "Help";
+            // 
+            // MenuItem_about
+            // 
+            this.MenuItem_about.Name = "MenuItem_about";
+            this.MenuItem_about.Size = new System.Drawing.Size(180, 22);
+            this.MenuItem_about.Text = "About";
+            this.MenuItem_about.Click += new System.EventHandler(this.MenuItem_about_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menu_main);
+            this.MainMenuStrip = this.menu_main;
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "ADGS";
+            this.menu_main.ResumeLayout(false);
+            this.menu_main.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menu_main;
+        private System.Windows.Forms.ToolStripMenuItem MenuItem_help;
+        private System.Windows.Forms.ToolStripMenuItem MenuItem_about;
+        private System.Windows.Forms.StatusStrip statusStrip1;
     }
 }
 
--- ADGS/ADGS/MainForm.cs
+++ ADGS/ADGS/MainForm.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ADGS
-{
+{ 
     public partial class MainForm : Form
     {
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_about_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Automatic data get system v 0.1", "ADGS", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
